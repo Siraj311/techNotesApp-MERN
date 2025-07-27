2a7730c8fefd6e0666d563545fcafdea4ec25b3c
Siraj Preena <sirajpreena311@gmail.com>
2025-07-27 16:29:19 +0530

    setting up pages and routing in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,35 @@
 import { Routes, Route } from 'react-router'
+import Layout from './components/Layout'
+import Public from './components/Public'
+import Login from './features/auth/Login'
+import DashLayout from './components/DashLayout'
+import Welcome from './features/auth/Welcome'
+import NotesList from './features/notes/NotesList'
+import UsersList from './features/users/UsersList'
 
 const App = () => {
   return (
-    <div>App</div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Public />} />
+        <Route path="login" element={<Login />} />
+
+        <Route path="dash" element={<DashLayout />}>
+
+          <Route index element={<Welcome />} />
+
+          <Route path="notes">
+            <Route index element={<NotesList />} />
+          </Route>
+
+          <Route path="users">
+            <Route index element={<UsersList />} />
+          </Route>
+
+        </Route> {/* End Dash Route */}
+
+      </Route>
+    </Routes>
   )
 }
 export default App
